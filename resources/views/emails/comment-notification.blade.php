e5012428773c242012746cdf6d501312c801564f
Blessedo <blessedvictorigwe@gmail.com>
2026-05-26 16:44:46 +0100

    lil change

--- resources/views/emails/comment-notification.blade.php
+++ resources/views/emails/comment-notification.blade.php
@@ -2,4 +2,4 @@
 <p>Hi Author, a new comment has been posted on your post: "{{ $comment->post->title }}"</p>
 <p>Commenter: {{ $comment->user->name }}</p>
 <p>Comment: {{ $comment->content }}</p>
-<a href=" http://127.0.0.1:8000/blog/{{ $comment->post->slug }}" target="_blank">View Post</a>
+<a href="{{ route('blog.show', $comment->post->slug) }}" target="_blank">View Post</a>
